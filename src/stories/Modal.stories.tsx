6d46ad1dca8673017ba0de639d9360eb5b77f730
Volodymyr Donets <volodons@gmail.com>
2023-12-22 17:10:19 +0200

    Create Storybook documentation for Modal Story

--- src/stories/Modal.stories.tsx
+++ src/stories/Modal.stories.tsx
@@ -1,29 +1,66 @@
 import React from 'react';
-import { Meta, Story } from '@storybook/blocks';
+import { Story, Meta } from '@storybook/blocks';
 
 import Modal, { ModalProps } from '../components/modal/modal';
 
 export default {
   title: 'Components/Modal',
   component: Modal,
+  argTypes: {
+    selectedPokemons: { control: 'array' },
+    onClose: { action: 'onClose' },
+  },
 } as Meta;
 
 const Template: Story<ModalProps> = (args: any) => <Modal {...args} />;
 
-export const Default = Template.bind({});
+export const Default: Story<ModalProps> = Template.bind({});
 Default.args = {
   selectedPokemons: [],
   onClose: () => console.log('Modal closed'),
 };
 
-export const Loading = Template.bind({});
+Default.parameters = {
+  docs: {
+    description: {
+      component: 'A modal component for displaying details of selected Pokémon.',
+    },
+    source: {
+      code: '<Modal selectedPokemons={[]} onClose={() => console.log("Modal closed")} />',
+    },
+  },
+};
+
+export const Loading: Story<ModalProps> = Template.bind({});
 Loading.args = {
   selectedPokemons: [],
   onClose: () => console.log('Modal closed'),
 };
 
-export const Empty = Template.bind({});
+Loading.parameters = {
+  docs: {
+    description: {
+      component: 'A modal component in a loading state.',
+    },
+    source: {
+      code: '<Modal selectedPokemons={[]} onClose={() => console.log("Modal closed")} />',
+    },
+  },
+};
+
+export const Empty: Story<ModalProps> = Template.bind({});
 Empty.args = {
   selectedPokemons: [],
   onClose: () => console.log('Modal closed'),
+};
+
+Empty.parameters = {
+  docs: {
+    description: {
+      component: 'A modal component with no selected Pokémon.',
+    },
+    source: {
+      code: '<Modal selectedPokemons={[]} onClose={() => console.log("Modal closed")} />',
+    },
+  },
 };
