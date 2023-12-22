d68b591f58ce87b94ad7b5dc50e3f3180dd195f0
Volodymyr Donets <volodons@gmail.com>
2023-12-22 18:52:56 +0200

    Refactor SubmitButton component

--- src/components/submitButton/submitButton.tsx
+++ src/components/submitButton/submitButton.tsx
@@ -9,7 +9,7 @@ const SubmitButton: React.FC<SubmitButtonProps> = ({ onSubmit }) => {
   return (
     <button
       className="w-40 flex items-center justify-between bg-blue-500 font-bold text-white p-2 rounded-md transition duration-300 ease-in-out transform hover:bg-blue-400 focus:outline-none focus:ring focus:border-blue-500"
-      type="button"
+      type="submit"
       onClick={onSubmit}
     >
       <StarIcon className="w-6 h-6" />
